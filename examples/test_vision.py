b9dc731e25a8fe72c27124b874a80af164025b9b
Nicolas Nahas <45595586+nicorne@users.noreply.github.com>
2025-02-24 08:14:11 +0100

    dependencies

--- examples/test_vision.py
+++ examples/test_vision.py
@@ -20,7 +20,7 @@ sys.path.append(project_root)
 ################### Adding Project Root to Python Path #############################
 
 from dratos import LLM, Agent
-from dratos import OpenAIEngine #, GoogleEngine
+from dratos import OpenAIEngine, GoogleEngine
 
 
 # def document(prompt: str):
