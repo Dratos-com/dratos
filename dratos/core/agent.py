a197b10620ce75cb2cae176b831d8fb1e1ac37fe
Nicolas Nahas <45595586+nicorne@users.noreply.github.com>
2025-02-26 19:49:20 +0100

    fix

--- dratos/core/agent.py
+++ dratos/core/agent.py
@@ -138,7 +138,10 @@ class Agent:
         try:
             model = self.response_model.__pydantic_validator__.validate_json(parameters, strict=True)
             logger.info(f"✅ Response format is valid")
-            return model
+            if self.json_response:
+                return parsed_json
+            else:
+                return model
         except Exception as e:
             logger.error(f"❌ Response format is not valid: {e}")
             raise e
@@ -179,7 +182,7 @@ class Agent:
             response = self.pydantic_validation(response)
 
         # Json response
-        if self.json_response:
+        if self.json_response and isinstance(response, str):
             response, _, _ = extract_json_from_str(response)
 
         return response
